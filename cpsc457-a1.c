93e9f6dcd0fe98f053eab98a0ae09288d76e7c21
flammabl3 <harryjung135@gmail.com>
2026-09-15 20:23:02 -0600

    Added IPC pipes

--- cpsc457-a1.c
+++ cpsc457-a1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 int fibonacci(int n) {
     if (n == 0)
@@ -16,17 +17,42 @@ int main(int argc, char* argv[]) {
     int fibonacci_n;
     int fib_result;
 
+    // pipe
+    int    pipefd[2];
+    char   buf[128];
+
     if (argc < 2) exit(0);
 
     for (int i = 1; i < argc; i++) {
+        pipe(pipefd);
         fr = fork();
         if (fr < 0) {
             printf("error!\n");
             exit(-1);
         } else if (fr == 0) {  
+            // close read file descriptor
+            close(pipefd[0]);
+
             fibonacci_n = atoi(argv[i]);
-            printf("Child Process (PID %d) F_{%d} = %d\n", getpid(), fibonacci_n, fibonacci(fibonacci_n));
+
+            // use sprintf to print string to buf
+            sprintf(buf, "Child Process (PID %d) F_{%d} = %d\n", getpid(), fibonacci_n, fibonacci(fibonacci_n));
+            // write buf to pipe write file descriptor
+            write(pipefd[1], &buf, strlen(buf));
+
+            // close pipe and exit child process
+            close(pipefd[1]);
             exit(0);
+        } else {
+            // not using the write file descriptor
+            close(pipefd[1]);
+            // read from file descriptor into buf and print!
+            while(read(pipefd[0], &buf, 1) > 0) {
+                printf(buf);
+            }
+            
+            // close pipe when done
+            close(pipefd[0]);
         }
     }
     
